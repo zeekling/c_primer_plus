d22453ea718ebba5820ca78603dee3bac8623d0f
zeekling <lingzhaohui@zeekling.cn>
2021-10-16 19:11:48 +0800

    add chapter3/altnames.c

---
+++ chapter3/altnames.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+int main(int argc, char const *argv[])
+{
+	int32_t me32;
+	me32 = 45933945;
+	printf("First, assume int32_t is int:");
+	printf("me32 = %d\n", me32);
+	printf("Next, let't not mak any assumption.\n");
+	printf("Instead, use a \"macro\" from inttype.h:");
+	printf("me32 = %" PRId32 "\n", me32);
+	return 0;
+}
